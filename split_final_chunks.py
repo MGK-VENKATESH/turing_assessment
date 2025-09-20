7d542ef0b4d5f92b582da25e3039e4e590d71ada
MGK venkatesh <139879845+MGK-VENKATESH@users.noreply.github.com>
2025-09-20 18:27:32 +0530

    Update split_final_chunks.py

--- split_final_chunks.py
+++ split_final_chunks.py
@@ -1,12 +1,12 @@
 import os
 
-# Folder with combined PDF texts
+
 input_folder = "data/combined"
-# Folder for smaller final chunks
+
 output_folder = "data/final_chunks"
 os.makedirs(output_folder, exist_ok=True)
 
-chunk_size = 1000  # number of words per chunk
+chunk_size = 1000  
 
 for file_name in os.listdir(input_folder):
     if file_name.endswith("_full.txt"):
@@ -14,7 +14,7 @@ for file_name in os.listdir(input_folder):
         with open(file_path, "r", encoding="utf-8") as f:
             words = f.read().split()
         
-        # Split into chunks
+        
         for i in range(0, len(words), chunk_size):
             chunk_words = words[i:i+chunk_size]
             chunk_text = " ".join(chunk_words)
