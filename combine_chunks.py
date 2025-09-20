9b0fb0613021796bf769a9946b3d20b74b863dc7
MGK venkatesh <139879845+MGK-VENKATESH@users.noreply.github.com>
2025-09-20 18:18:20 +0530

    Update combine_chunks.py

--- combine_chunks.py
+++ combine_chunks.py
@@ -1,22 +1,22 @@
 import os
 import glob
 
-# Create output folder if it doesn't exist
+
 os.makedirs("data/combined", exist_ok=True)
 
-# Get all chunk files
+
 chunk_files = glob.glob("data/chunks/*.txt")
 
-# Group chunks by PDF base name
+
 pdf_dict = {}
 for file in chunk_files:
-    # remove _chunkXX.txt from filename to get base PDF name
+    
     base_name = "_".join(os.path.basename(file).split("_chunk")[:-1])
     pdf_dict.setdefault(base_name, []).append(file)
 
-# Combine chunks for each PDF
+
 for pdf_name, files in pdf_dict.items():
-    files.sort()  # make sure chunks are in order
+    files.sort()  
     output_file = f"data/combined/{pdf_name}_full.txt"
     with open(output_file, "w", encoding="utf-8") as outfile:
         for f in files:
